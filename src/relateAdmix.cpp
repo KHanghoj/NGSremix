6b7f78ee1e444aef86ce096b9e90d0eb460a9ef6
khanghoj <kristianhanghoej@gmail.com>
2021-01-12 22:22:13 +0100

    add ref

--- src/relateAdmix.cpp
+++ src/relateAdmix.cpp
@@ -4,7 +4,7 @@
 
 // //checktime
 // #include <sys/time.h>
-
+//https://stackoverflow.com/a/27448980
 float timedifference_msec(struct timeval t0, struct timeval t1)
 {
     return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
